aa0875b12edd29acc710c84abb6ec7b8ee25e585
Darren Vong <d.vongy@gmail.com>
2019-12-11 23:35:51 +0000

    Add error handling for main program logic
    Change run to return Result too.
    Handles error using `if let` rather than calling `unwrap_or_else` as
    we don't care about `run`'s return value.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 struct Config {
     query: String,
@@ -23,11 +24,12 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
     
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 fn main() {
@@ -41,5 +43,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
