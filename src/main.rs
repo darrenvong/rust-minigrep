3d93acd73bf99f3b06bd7926d1564d08c19685e6
Darren Vong <d.vongy@gmail.com>
2019-12-11 22:51:19 +0000

    Extract argument parser into a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
 use std::fs;
 
+fn parse_configs(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_configs(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
