bc645b6730ac411dfff9060a493d3be45b2b0521
Darren Vong <d.vongy@gmail.com>
2019-12-11 23:01:38 +0000

    Group args values into a Config struct
    This allows us to convey the meaning of the two variables better -
    they're related and used to configure how the program runs.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 use std::env;
 use std::fs;
 
-fn parse_configs(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String
+}
 
-    (query, filename)
+fn parse_configs(args: &[String]) -> Config {
+    // Calling .clone() is needed so that Config can own the values of `query`
+    // and `filename` without violating the borrowing rule.
+    //
+    // Not the most efficient solution, but ok for now for simplicity.
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_configs(&args);
+    let config = parse_configs(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
     
     println!("With text:\n{}", contents);
