3eca6fef0d4c969f5eb8d16bda05a6f7a015b57b
Darren Vong <d.vongy@gmail.com>
2019-12-11 23:19:23 +0000

    Add error handling for parsing configs
    Config::new now returns a Result, allowing the running main code to deal
    with errors more cleanly.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,7 +8,10 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         // Calling .clone() is needed so that Config can own the values of `query`
         // and `filename` without violating the borrowing rule.
         //
@@ -15,14 +19,17 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
