b9a6c026369d3f0cd8bc021993e0b47c114efafe
Darren Vong <d.vongy@gmail.com>
2019-12-11 23:08:34 +0000

    Rename parse_configs to Config::new
    This effectively moves the function into the struct, since it's creating
    instances of Config in the first place.
    Moving (renaming) the function allows us to tie it to Config to better
    express the function's intention.

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,23 @@ struct Config {
     filename: String
 }
 
-fn parse_configs(args: &[String]) -> Config {
-    // Calling .clone() is needed so that Config can own the values of `query`
-    // and `filename` without violating the borrowing rule.
-    //
-    // Not the most efficient solution, but ok for now for simplicity.
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // Calling .clone() is needed so that Config can own the values of `query`
+        // and `filename` without violating the borrowing rule.
+        //
+        // Not the most efficient solution, but ok for now for simplicity.
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_configs(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
